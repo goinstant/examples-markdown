f8d75e011c4786ab365042744cbbf549c7f1c342
Jason Chen <jhchen7@gmail.com>
2014-03-10 18:50:37 -0700

    side by side markdown rendering working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   var editor = ace.edit('ace-container');
   editor.setTheme("ace/theme/monokai");
-  editor.getSession().setMode('ace/mode/markdown');
+  var session = editor.getSession()
+  session.setMode('ace/mode/markdown');
+
+  $markdownContainer = $('#markdown-container');
+
+  session.on('change', function() {
+    var html = markdown.toHTML(session.getValue());
+    $markdownContainer.html(html);
+  });
 });
