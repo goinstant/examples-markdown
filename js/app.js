b3f2206eee1a600209516bc6cb3215627183129f
Jason Chen <jhchen7@gmail.com>
2014-03-10 19:04:33 -0700

    style markdown output

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   var editor = ace.edit('ace-container');
   editor.setTheme("ace/theme/monokai");
-  var session = editor.getSession()
+  var session = editor.getSession();
   session.setMode('ace/mode/markdown');
+  session.setUseSoftTabs(true);
 
   $markdownContainer = $('#markdown-container');
 
